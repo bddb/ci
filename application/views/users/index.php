3933a5161eaaaa3a3c4b18f169197da5778b3edd
Lillian <bddb_tw@yahoo.com.tw>
2013-04-25 11:41:29 +0800

    search

--- application/views/users/index.php
+++ application/views/users/index.php
@@ -2,6 +2,7 @@
 <a href="<?= site_url('users/add')?>">新增使用者</a>
 <a href="<?= site_url('users/edit')?>">修改使用者</a>
 <a href="<?= site_url('users/delete')?>">刪除使用者</a>
+<a href="<?= site_url('users/search')?>">查詢使用者</a>
 <hr>
 <?
   foreach ($users as $user) {
