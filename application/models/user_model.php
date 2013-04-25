5c83ca5137738fb03b3327b2710ff37780a11a47
Lillian <bddb_tw@yahoo.com.tw>
2013-04-25 11:32:17 +0800

    Revert "Revert "init""
    This reverts commit ea418f5416d222e73a7ddfc9c26dd4dcb5f4fe5e.

---
+++ application/models/user_model.php
@@ -0,0 +1,32 @@
+<?php
+class user_model extends CI_Model{
+	 function __construct()
+    {
+        // 呼叫模型(Model)的建構函數
+        parent::__construct();
+    }
+    
+    function all(){
+        $query = $this->db->get('users');
+        return $query->result();
+    }
+
+    function get_user($id){
+        $this->db->where("id" , $id);
+        $query = $this->db->get('users');
+        return $query->result();
+    }
+
+    function create($data){
+        return $this->db->insert('users', $data); 
+    }
+	
+	function update($data){
+	
+	}
+	
+	function delete($data){
+		
+	}
+}
+?>
---
+++ application/views/users/delete.php
@@ -0,0 +1,5 @@
+<h2>刪除使用者</h2>
+<form action="<?= site_url('users/deleted')?>" method="post">
+請選擇要刪除的使用者ID:<input name="deleteid" type="text"><br/>
+<input type="submit">
+</form>
