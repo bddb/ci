ea418f5416d222e73a7ddfc9c26dd4dcb5f4fe5e
Lillian <bddb_tw@yahoo.com.tw>
2013-04-25 11:25:27 +0800

    Revert "init"
    This reverts commit 8e9a84e4ea8b6f394ebf4e35c84525402e05195b.

--- application/views/users/edit.php
+++
@@ -1,9 +0,0 @@
-<h2>修改使用者</h2>
-<form action="<?= site_url('users/edit')?>" method="post">
-ID : <input name="id" type="text"><br/>
-Name : <input name="name" type="text"><br/>
-Tel : <input name="tel" type="text"><br/>
-Created time : <input name="created_at" type="text"><br/>
-Update time : <input name="updated_at" type="text"><br/>
-<input type="submit">
-</form>
